7bde2b29f1b885ddf0e75b5ddbe28926d25010e1
CodeSandbox <support@codesandbox.io>
2026-01-04 11:14:24 +0000

    Sandbox snapshot commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,16 @@ function App() {
   };
   return (
     <main className="flex flex-col items-center gap-6 py-16 max-w-[1280px] mx-auto">
-      <h1 style={headingStyle}>Hello React & Tailwind!</h1>
       <div className="flex flex-row items-center gap-6">
+        <h1>Hello React & Tailwind!</h1>
         <button
           className="bg-sky-300 px-3 py-2 text-blue-600 bg-red-600 rounded hover:bg-sky-400"
-          onClick={() => setCount(count + 2)}
+          onClick={(e) => {
+            let element = e.target.parentElement.style;
+            //console.log(element.alignContent);
+            element.color = headingStyle.color;
+            element.backgroundColor = headingStyle.backgroundColor;
+          }}
         >
           Count: {count}
         </button>
