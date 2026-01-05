28553bd4fb8462e5a329d410818e0e65a51efd00
CodeSandbox <support@codesandbox.io>
2026-01-05 06:32:14 +0000

    Sandbox snapshot commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Hello from "./Hello.jsx";
+function App() {
+  return (
+    <main className="flex flex-rows items-center gap-2 py-2  mx-auto border-[2px] border-[wheat]">
+      <Hello name="alasdasia" email="asfdsgh" />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Hello.jsx
@@ -0,0 +1,11 @@
+function Hello({ name, email }) {
+  return (
+    <h1>
+      Hello {name}
+      <br />
+      {email}
+    </h1>
+  );
+}
+
+export default Hello;
