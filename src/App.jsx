34db9c12c0d4c9e420a0c69ec8e78684c6ff2bd5
CodeSandbox <support@codesandbox.io>
2026-01-07 05:24:48 +0000

    Sandbox snapshot commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Hello from "./Hello.jsx";
 function App() {
   return (
     <main className="flex flex-rows items-center gap-2 py-2  mx-auto border-[2px] border-[wheat]">
-      <Hello name="alasdasia" email="asfdsgh" />
+      <Hello />
     </main>
   );
 }
--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,10 +1,39 @@
-function Hello({ name, email }) {
+import { useState } from "react";
+
+function Hello() {
+  let [email, setEmail] = useState("");
+  let [password, setpassword] = useState("");
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email + "  " + password);
+  };
   return (
-    <h1>
-      Hello {name}
-      <br />
-      {email}
-    </h1>
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center gap-4 w-full"
+      >
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border border-gray-500 rounded-sm w-[30%] py-2"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setpassword(e.target.value)}
+          className="border border-gray-500 rounded-sm w-[30%] py-2"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-900 "
+        >
+          submit
+        </button>
+      </form>
+    </>
   );
 }
 
