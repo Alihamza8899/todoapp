15c43eb6c165cdd6ae36aaeebe9e56cc057af62f
CodeSandbox <support@codesandbox.io>
2026-01-07 06:16:42 +0000

    Sandbox snapshot commit

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 
 function Hello() {
-  let [email, setEmail] = useState("");
-  let [password, setpassword] = useState("");
+  let [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  let changeValue = (e) => {
+    let { type, value } = e.target;
+    let prevData = formData;
+    setFormData({ ...prevData, [type]: value });
+  };
 
   let handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email + "  " + password);
+    console.log(formData);
   };
   return (
     <>
@@ -14,18 +21,35 @@ function Hello() {
         onSubmit={handleSubmit}
         className="flex flex-col items-center gap-4 w-full"
       >
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="border border-gray-500 rounded-sm w-[30%] py-2"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setpassword(e.target.value)}
-          className="border border-gray-500 rounded-sm w-[30%] py-2"
-        />
+        <label>
+          <input
+            checked={formData.gender == "male"}
+            type="radio"
+            name="gender"
+            value="male"
+          />
+          Male
+        </label>
+        <label>
+          <input
+            checked={formData.gender == "male"}
+            type="radio"
+            name="gender"
+            value="female"
+          />
+          Female
+        </label>
+        <label>
+          <input checked={formData.agree} type="checkbox" name="agree" />I agree
+        </label>
+        <label>
+          <select name="country">
+            <option value="pakistan">pakistan</option>
+            <option value="usa">usa</option>
+            <option value="india">india</option>
+            <option value="germany">germany</option>
+          </select>
+        </label>
         <button
           type="submit"
           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-900 "
