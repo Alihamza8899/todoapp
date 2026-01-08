dcb1e21d9f57c73e9cd8a32942774a92731388bb
CodeSandbox <support@codesandbox.io>
2026-01-08 08:51:40 +0000

    Sandbox snapshot commit

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,18 +1,17 @@
 let TodoApp = () => {
   return (
     <div className="min-w-[40%] border border-[black] m-16 p-8 rounded-lg bg-white ">
-      <div>
+      <div className="pb-4">
         <input
           type="text"
-          className="w-[80%] p-2  border-2 border-black rounded-md focus:border-yellow-500 focus:outline-none transition-all duration-3000"
+          className="w-[75%] p-2  border-2 border-black rounded-md focus:border-yellow-500 focus:outline-none transition-all duration-3000"
         />
-        <button className="text-white bg-yellow-500 px-2 py-[10px] ml-2 ">
+        <button className="text-white bg-yellow-500 px-2 py-[9px] ml-2 rounded-md hover:bg-white border-2 border-yellow-500 hover:text-black">
           Enter
         </button>
       </div>
-      <div>
-        <h3>To do list</h3>
-      </div>
+      <hr />
+      <div></div>
     </div>
   );
 };
