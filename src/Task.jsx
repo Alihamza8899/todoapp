a8aa466877a22e4fc243fea69a86ab538ddaea24
CodeSandbox <support@codesandbox.io>
2026-01-09 14:41:15 +0000

    Sandbox snapshot commit

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,7 +1,7 @@
-let Task = () => {
+let Task = ({ task }) => {
   return (
-    <div className="flex justify-between p-2  bg-[wheat]">
-      <h3>hello</h3>
+    <div className="flex justify-between p-2 my-2 bg-[wheat]">
+      <h3>{task}</h3>
       <div>
         <button className="bg-red-600 py-[2px] px-2 text-white ml-2 rounded-md">
           &#x2717;
--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -6,15 +6,12 @@ let TodoApp = () => {
   let [taskList, setTaskList] = useState([]);
 
   let handleSubmit = (e) => {
-    e.preventDefault();
     setTask(e.target.value);
-    setTaskList([
-      ...taskList,
-      {
-        Task: e.target.value,
-      },
-    ]);
-    console.log(taskList);
+  };
+  let addTask = (e) => {
+    e.preventDefault();
+    setTaskList([...taskList, task]);
+    setTask("");
   };
   return (
     <div className="min-w-[40%] border border-[black] m-16 p-8 rounded-lg bg-white ">
@@ -25,14 +22,19 @@ let TodoApp = () => {
           onChange={handleSubmit}
           className="w-[75%] p-2  border-2 border-black rounded-md focus:border-yellow-500 focus:outline-none transition-all duration-3000"
         />
-        <button className="text-white bg-yellow-500 px-2 py-[9px] ml-2 rounded-md hover:bg-white border-2 border-yellow-500 hover:text-black">
+        <button
+          onClick={addTask}
+          className="text-white bg-yellow-500 px-2 py-[9px] ml-2 rounded-md hover:bg-white border-2 border-yellow-500 hover:text-black"
+        >
           Enter
         </button>
       </div>
       <hr />
       <div className="p-2 mt-2">
         <h1 className="font-[4px] p-2 ">Tasks:-</h1>
-        <Task />
+        {taskList.map((val, i) => {
+          return <Task task={val} key={i} />;
+        })}
       </div>
     </div>
   );
