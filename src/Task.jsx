6f7a6ec20182ebc19032d5958e0086ac858fd79a
CodeSandbox <support@codesandbox.io>
2026-01-09 06:09:03 +0000

    Sandbox snapshot commit

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,7 +1,15 @@
 let Task = () => {
   return (
-    <div>
+    <div className="flex justify-between p-2  bg-[wheat]">
       <h3>hello</h3>
+      <div>
+        <button className="bg-red-600 py-[2px] px-2 text-white ml-2 rounded-md">
+          &#x2717;
+        </button>
+        <button className="bg-green-600 py-[2px] px-2 text-white ml-2 rounded-md">
+          &#x2714;
+        </button>
+      </div>
     </div>
   );
 };
--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import Task from "./Task.jsx";
 
 let TodoApp = () => {
+  let [task, setTask] = useState("");
+  let [taskList, setTaskList] = useState([]);
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    setTask(e.target.value);
+    setTaskList([
+      ...taskList,
+      {
+        Task: e.target.value,
+      },
+    ]);
+    console.log(taskList);
+  };
   return (
     <div className="min-w-[40%] border border-[black] m-16 p-8 rounded-lg bg-white ">
       <div className="pb-4">
         <input
           type="text"
+          value={task}
+          onChange={handleSubmit}
           className="w-[75%] p-2  border-2 border-black rounded-md focus:border-yellow-500 focus:outline-none transition-all duration-3000"
         />
         <button className="text-white bg-yellow-500 px-2 py-[9px] ml-2 rounded-md hover:bg-white border-2 border-yellow-500 hover:text-black">
@@ -14,7 +31,7 @@ let TodoApp = () => {
       </div>
       <hr />
       <div className="p-2 mt-2">
-        <h1>Tasks:-</h1>
+        <h1 className="font-[4px] p-2 ">Tasks:-</h1>
         <Task />
       </div>
     </div>
